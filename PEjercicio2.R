37c4b8122c05017a9b48ac0b8ed8a431bc137760
naimmanriquez <66890535+naimmanriquez@users.noreply.github.com>
2020-08-15 15:32:28 -0600

    Add files via upload

---
+++ PEjercicio2.R
@@ -0,0 +1,11 @@
+## La extracción de dos bolas de una urna, que contiene 4 bolas blancas y 3 bolas negras
+## La probabilidad de sacar una bola blanca y una negra
+
+## La opción de R para esto es choose:
+
+choose(4, 1) * choose(3, 1) / choose(7, 2)
+
+## Tenemos 7 bolas en total y 2 oportunidades
+## Tenemos 4 bolas blancas y buscamos la probabilidad de tener una
+## Tenemos 3 bolas negras y buscamos la probabilidad de tener una tambien.
+
